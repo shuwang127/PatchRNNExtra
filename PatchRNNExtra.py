e3eebbcdb04b5e69556eb5c3e84d1873f1cd7cf0
Shu Wang <shuwang127@yahoo.com>
2020-09-23 18:33:41 -0400

    Add files via upload

--- PatchRNNExtra.py
+++ PatchRNNExtra.py
@@ -1,25 +1,28 @@
 '''
-    SecurityPatchIdentificationRNN: Security Patch Identification using RNN model.
+    PatchRNNExtra: RNN-based Security Patch Identification with Oversampling Samples.
     Developer: Shu Wang
     Date: 2020-09-23
     Version: S2020.09.23-V4-Extra
-    Description: patch identification using both commit messages and normalized diff code.
+    Description: patch identification using both commit messages and normalized diff code
+                 with extra oversampling samples.
     File Structure:
-    SecurityPatchIdentificationRNN
-        |-- analysis                                # task analysis.
+    PatchRNNExtra
         |-- data                                    # data storage.
                 |-- negatives                           # negative samples.
                 |-- positives                           # positive samples.
                 |-- security_patch                      # positive samples. (official)
+        |-- synthesis                               # synthetic data storage.
+                |-- negatives                           # negative samples. (synthetic)
+                |-- positives                           # positive samples. (synthetic)
+                |-- security_patch                      # positive samples. (synthetic)
         |-- temp                                    # temporary stored variables.
                 |-- data.npy                            # raw data. (important)
                 |-- props.npy                           # properties of diff code. (important)
                 |-- msgs.npy                            # commit messages. (important)
                 |-- ...                                 # other temporary files. (trivial)
-        |-- SecurityPatchIdentificationRNN.ipynb    # main entrance. (Google Colaboratory)
-        |-- SecurityPatchIdentificationRNN.py       # main entrance. (Local)
+        |-- PatchRNNExtra.py                        # main entrance. (Local)
     Usage:
-        python SecurityPatchIdentificationRNN.py
+        python PatchRNNExtra.py
     Dependencies:
         clang >= 6.0.0.2
         torch >= 1.2.0+cu92
